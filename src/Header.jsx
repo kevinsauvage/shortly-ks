17e8faec0ecade1af3166f1491ceaac711f6717e
kevin sauvage <kevinsauvage@outlook.com>
2021-02-10 18:43:10 +0100

    adding style and changing animation header navigation

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import Logo from "./assets/images/logo.svg";
 import Button from "./Button";
 import "./Header.css";
@@ -6,6 +6,8 @@ import { AppContext } from "./AppContext";
 
 const Header = () => {
   const props = useContext(AppContext);
+  const headerRight = useRef(null);
+  const headerLeft = useRef(null);
 
   useEffect(() => {
     window.onscroll = () => {
@@ -13,14 +15,32 @@ const Header = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (props.menuIsOpen) {
+      body.style.overflow = "hidden";
+    } else {
+      body.style.overflow = "scroll";
+    }
+  }, [props.menuIsOpen]);
+
+  const handleClickOutsideNav = (e) => {
+    if (
+      !headerLeft.current.contains(e.target) &&
+      !headerRight.current.contains(e.target)
+    ) {
+      props.setMenuIsOpen(false);
+    }
+  };
   return (
     <div
+      onClick={handleClickOutsideNav}
       className={
         props.menuIsOpen
           ? "header__container menu-is-open flex-jc-sb-ai-center"
           : "header__container flex-jc-sb-ai-center"
       }>
-      <div className="header__left">
+      <div ref={headerLeft} className="header__left">
         <img src={Logo} alt="logo" className="header__left__logo" />
         <nav className="header__left__nav flex-ai-c">
           <li>Features</li>
@@ -28,7 +48,7 @@ const Header = () => {
           <li>Ressources</li>
         </nav>
       </div>
-      <div className="header__right">
+      <div ref={headerRight} className="header__right">
         <ul className="header__right__btn__container flex-ai-c">
           <li>Login</li>
           <li className="header__right__btn__signUp">
